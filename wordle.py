4e1fe43d469760561b812d3394fc961373f51759
Herpinho <149377061+Herpinho@users.noreply.github.com>
2026-01-07 22:01:49 +0000

    Wordle Done
    Wordle has been coded.

---
+++ wordle.py
@@ -0,0 +1,90 @@
+import random
+import time
+import sys
+with open('valid-wordle-words.txt', 'r') as f:
+    word_list = [line.strip() for line in f]
+with open('shuffled_real_wordles.txt', 'r') as f:
+    answer_list = [line.strip() for line in f]
+def start():
+    answer = answer_list[random.randint(1,2316)]
+    answer_word_list = [letter.strip() for letter in answer]
+    return answer_word_list
+win_counter = 0
+
+def print_color_coded_words(letters, scores):
+    sys.stdout.write("\033[F")
+    sys.stdout.flush()
+    bg_colors = {
+        1: "\033[48;5;242m", 
+        2: "\033[43m",       
+        3: "\033[42m",       
+    }
+    reset = "\033[0m"
+    
+    result_string = ""
+    
+    for i in range(len(letters)):
+        sys.stdout.write(f"\033[{i*3}D")
+        sys.stdout.write("\033[K")
+        sys.stdout.flush()
+        char = letters[i].upper()
+        color = bg_colors.get(scores[i], "")
+
+        result_string += f"{color} {char} {reset}"
+    
+        sys.stdout.write(result_string)
+        sys.stdout.flush()
+        time.sleep(0.4)
+
+
+
+def result(letter_list,answer_word_list):
+    pos = 0
+    points = []
+    for letter in letter_list:
+        if letter == answer_word_list[pos]:
+            points.append(3)
+        elif letter in answer_word_list:
+            points.append(2)
+        else:
+            points.append(1)
+        pos +=1
+    return points
+def game():
+    global win_counter
+    current_answer = start()
+    guess_count = 0
+    while guess_count < 6:
+        guess_count+=1
+        guess = input()
+        if len(guess) == 5:
+            letter_list = [letter.strip() for letter in guess]
+            points = result(letter_list,current_answer)
+            print_color_coded_words(letter_list, points)
+            if sum(points) == 15:
+                win = True
+                break
+        else:
+            guess_count-=1
+        sys.stdout.write("\033[B")
+        sys.stdout.write("\033[15D")
+        sys.stdout.flush()
+
+
+
+    if win == True:
+        win = False
+        win_counter +=1
+        print(f"""You Won, good job, wanna play again? (Y/N)
+Win counter: {win_counter}   
+""")
+    else:
+        print("You lost, wanna play again?(Y/N)")
+    option = input()
+    if option == "Y":
+        game()
+    if option== "N":
+        exit()    
+
+if __name__  == "__main__":
+    game()    
